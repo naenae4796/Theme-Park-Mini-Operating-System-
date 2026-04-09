205ad0323ea32660f37ac7b3b1a6c82f0a41308f
Nicholas Mora <nmora4874@gmail.com>
2026-04-09 15:04:18 -0700

    added function prototypes for guest functions.

--- osproject/osproject/guest.h
+++ osproject/osproject/guest.h
@@ -11,4 +11,12 @@ struct guest {
 	char state;
 };
 
+//function prototypes
+void initializeGuest(guest& g, int id, int arrival, int burst, int memory,
+	int prio, int ride); //initializes guest with data
+
+void displayGuest(const guest& g); //shows guest and data
+
+void updateRemainingTime(guest& g);
+
 #endif
